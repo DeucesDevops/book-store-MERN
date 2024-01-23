428ee53a3afd10b146c135218a0b91203357e842
DeucesDevops <132962088+DeucesDevops@users.noreply.github.com>
2024-01-23 22:51:20 +0000

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL =  'mongodb+srv://macroni607:Deuces.607@cluster0.8cd3fa1.mongodb.net/books-collection?retryWrites=true&w=majority';
+export const mongoDBURL =  ****
